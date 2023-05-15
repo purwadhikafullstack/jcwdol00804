a1ebef97dcd2fe19394d8b697d3b10e5328876ec
High Call <49810289+haikal1223@users.noreply.github.com>
2023-05-15 16:34:55 +0700

    [PWD-ENV] update env (#4)

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,5 +1,5 @@
-const path = require("path")
-require("dotenv").config({path: path.resolve(__dirname, "../.env")})
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const cors = require("cors");
 const { join } = require("path");
