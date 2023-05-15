65e3f06ba669dd35f584f4aab8c5b8490271cc3a
haikal1223 <49810289+haikal1223@users.noreply.github.com>
2023-05-15 16:13:30 +0700

    [PWD-FIX] update mysql version

--- projects/server/src/config/db.js
+++ projects/server/src/config/db.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql");
+const mysql2 = require("mysql2");
 const util = require("util");
 
-const db = mysql.createPool({
+const db = mysql2.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
